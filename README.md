c57ba2a130a0cd938609d4f50e44f5db70abb123
john mauran <johnmauran@johns-MacBook-Air.local>
2017-06-22 10:33:01 -0400

    working script for windows

--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 Use the music organizer right after downloading an album / music release archive
 file to put all of its contents into your music folder in your file directory
 and into iTunes. Working on getting it into WinAmp...
+
+This will only work if you run the script immediately after downloading the zip / rar
--- music_organizer.py
+++ music_organizer.py
@@ -4,40 +4,87 @@
 
 import shutil
 import os
-import eyeD3
+from eyed3 import id3
 import glob
-import ZipFile
-tag = eyeD3.Tag()
+import zipfile
+import stat
+import rarfile
+import unrar
+import patoolib
+
 
 ## create zipfile object of the downloaded album and get a tracklist
 
-downloads = glob.glob('/Users/johnmauran/Downloads/*')
+rarfile.UNRAR_TOOL=r'C:\Users\John\AppData\Local\Programs\Python\Python36-32\UnRAR.exe'
+tag = id3.Tag()
+
+downloads = glob.glob("C:\\Users\\John\\Downloads\\*")
 music_zip = max(downloads, key=os.path.getctime)
-music_folder = zipfile.ZipFile(music_zip)
-trax = music_folder.namelist()
+if os.path.splitext(music_zip)[-1] == '.zip':
+    music_folder = zipfile.ZipFile(music_zip)
+elif os.path.splitext(music_zip)[-1] == '.rar':
+    music_folder = rarfile.RarFile(music_zip)
+temporary_album_folder = r'C:\\Users\\John\\Downloads\\temporary_album_folder'
+if not os.path.exists(temporary_album_folder):
+    os.makedirs(temporary_album_folder)
 
-## get the Artist and Album as strings
 
-tag.link(trax[0])
-artist = tag.getArtist()
-album = tag.getAlbum()
+## extract the rar / zip into a temporary folder
+os.chdir(temporary_album_folder)
+music_folder.extractall()
+music_folder.close()
+print(temporary_album_folder)
 
-## make a place for the album to go
+## check if the rar / zip is an album folder or just the contents of the album
+## then get the artist and album from the first track
+temp_list = os.listdir(temporary_album_folder)
+if os.path.isdir(temp_list[0]):
+    new_temp_list = os.listdir(temp_list[0])
+    song = new_temp_list[0]
+    pathhelp = temp_list[0]
+else:
+    song = temp_list[0]
+    pathhelp = temporary_album_folder
+songpath_help = os.path.join(pathhelp, song)
+songpath = os.path.join(r'C:\Users\John\Downloads\temporary_album_folder', songpath_help)
+tag.parse(songpath)
+print(tag.artist)
+print(tag.album_artist)
+print(tag.album)
+if not tag.artist:
+    artist = tag.album_artist
+else:
+    artist = tag.artist
+album = tag.album
 
-newpath = f"/Users/johnmauran/Music/{artist}/{album}"
+## make the destination paths for the album
+newpath = f"C:/Users/John/Music/Music/{artist}/{album}"
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
-## extract the contents of the album and
-## and place them in the folder under Music
+iTunesPath = f"C:/Users/John/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized/{artist}/{album}"
+if not os.path.exists( iTunesPath):
+    os.makedirs( iTunesPath )
 
-os.chdir(newpath)
-music_folder.extractall()
-music_folder.close()
 
-## and add them to iTunes as well
+## this is the script for: if the rar / zip contains an album folder
+if os.path.isdir(temp_list[0]):
+    path_helper = os.path.join(temporary_album_folder, temp_list[0])
+    for file in new_temp_list:
+        full_file = os.path.join(path_helper, file)
+        if (os.path.isfile(full_file)):
+            shutil.copy(full_file, newpath)
+            shutil.copy(full_file, iTunesPath)
 
-iTunesPath = f"/Users/johnmauran/Music/iTunes/iTunes Media/Automatically Add to iTunes.localized/{artist}/{album}"
-if not os.path.exists(iTunesPath)
-    os.makedirs(iTunesPath)
-shutil.copytree( newpath, iTunesPath)
+## this is the script for: if the rar / zip contains just the contents of the album
+else:
+    for file in temp_list:
+        full_file = os.path.join( temporary_album_folder, file)
+        if (os.path.isfile(full_file)):
+            shutil.copy(full_file, newpath)
+            shutil.copy(full_file, iTunesPath)
+
+
+## deletes the temporary work folder
+os.chdir("C:\\Users\\John\\Downloads")
+shutil.rmtree(     temporary_album_folder)
